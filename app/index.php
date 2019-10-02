3619d8a126bab958c5e604fcacce990cc3ff30bd
kavich <kavich@ya.ru>
2019-10-02 15:32:20 +0300

    init

---
+++ app/index.php
@@ -0,0 +1,6 @@
+<?php
+//var_dump(getenv('PHP_MYSQL_USER'));
+//var_dump($GLOBALS);
+$l = mysqli_connect(getenv('PHP_MYSQL_HOST'),getenv('PHP_MYSQL_USER'),getenv('PHP_MYSQL_PASSWORD'), getenv('PHP_MYSQL_DB'));
+//var_dump($l);
+phpinfo();
